c97e0eb063c2c0c6d16aadcb281153fdcdd3e871
Brandon Eleonora <brandoneleonora38@gmail.com>
2023-08-12 18:48:00 -0400

    complete project

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
-# your code goes here!
+
+
+
+class Anagram:
+    def __init__(self, word):
+        self._word = word
+
+    def match(self, word_list):
+        result = []
+        for l in word_list:
+            sorted([letter for letter in l])
+            if sorted([letter for letter in self._word]) == sorted([letter for letter in l]):
+                result.append(l)
+        return result 
+        
